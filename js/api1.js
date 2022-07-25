8009e46033d033afc452d40202cfa9b9fc18310d
Razmik <razmik.minasyann@gmail.com>
2022-07-25 13:51:43 +0400

    add export

--- js/api1.js
+++ js/api1.js
@@ -2,3 +2,5 @@ import http from 'k6/http';
 import { authenticateUsingAih } from './oauth/oauth.js'
 import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
+
+export const options = {
