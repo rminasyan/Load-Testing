17533435075370d43a1dfffbf2a853915f6cde66
Razmik <razmik.minasyann@gmail.com>
2022-07-25 13:50:56 +0400

    add import

--- js/api1.js
+++ js/api1.js
@@ -1,2 +1,3 @@
 import http from 'k6/http';
 import { authenticateUsingAih } from './oauth/oauth.js'
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
