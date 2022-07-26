c9fa9ca2120bc0f5ee3543cce75cc0db56c887ba
Razmik Minasyan <razmik.minasyann@gmail.com>
2022-07-26 11:18:39 +0400

    add script

--- js/api1.js
+++ js/api1.js
@@ -4,3 +4,4 @@ import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporte
 import { jUnit, textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 
 export const options = {
+    stages: [
